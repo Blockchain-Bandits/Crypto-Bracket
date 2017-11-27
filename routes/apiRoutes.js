c9215478df9b8615da682d39d569d7acd6d4eb5b
Richard Lieu <CeciliaWang@Cecilia-Ximeng-Wangs-MacBook-Pro.local>
2017-11-27 13:25:06 -0800

    pull

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+var bittrex = require('node-bittrex-api');
 var connection = require("../config/connection.js");
 var user = 1;
 
@@ -6,16 +7,59 @@ var user = 1;
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+    var price;
+    bittrex.getticker( { market : 'USDT-BTC' }, function( data, err ) {
+        price = data.result.Last;
+    });
+    
+    app.get("/api/transactions/:coin/:method", function(req, res) {
+        var coin = req.params.coin;
+        var coinPrice;
+        bittrex.getticker( { market : `BTC-${coin}` }, function( data, err ) {
+            coinPrice = data.result.Last * price;
 
-    app.get("/api/transactions/:coin", function(req, res) {
+            if (req.params.method === 'fifo') {
+                connection.query(
+                    "SELECT * FROM transactionsFIFO WHERE user_id = ? AND coin = ?", 
+                    [user, coin],
+                    function(err, data) {
+                        if (err) throw err;
+                        data.push({currentPrice: coinPrice});
+                        res.json(data);
+                    }
+                );
+            } else if (req.params.method === 'lifo') {
+                connection.query(
+                    "SELECT * FROM transactionsLIFO WHERE user_id = ? AND coin = ?", 
+                    [user, coin],
+                    function(err, data) {
+                        if (err) throw err;
+                        data.push({currentPrice: coinPrice});
+                        res.json(data);
+                    }
+                );
+            } else if (req.params.method === 'avg') {
+                connection.query(
+                    "SELECT * FROM transactionsAvg WHERE user_id = ? AND coin = ?", 
+                    [user, coin],
+                    function(err, data) {
+                        if (err) throw err;
+                        data.push({currentPrice: coinPrice});
+                        res.json(data);
+                    }
+                );
+            }
+        });
+        
+    });
+    app.get("/api/transactions", function(req, res) {
         connection.query(
-            "SELECT * FROM transactionsLIFO WHERE user_id = ? AND coin = ?", 
-            [user, req.params.coin],
+            "SELECT coin FROM transactionsFIFO WHERE user_id = 1 GROUP BY coin", 
             function(err, data) {
                 if (err) throw err;
                 res.json(data);
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
