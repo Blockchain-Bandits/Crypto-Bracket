6e50879e43b73319371106edcf11f453ea2b68f8
Fausto Rendon <faustor001@gmail.com>
2017-12-07 21:10:41 -0800

    Add comments for ccxt api

--- controllers/ccxt.js
+++ controllers/ccxt.js
@@ -8,10 +8,30 @@ var ccxt = require("../models/ccxt-model.js");
 
 module.exports = function(app) {
     //**********************************
+    // SEE MANUAL: https://github.com/ccxt/ccxt/wiki/Manual
+    //'timeframes': {      // empty if the exchange !hasFetchOHLCV
+    //     '1m': '1minute',
+    //     '1h': '1hour',
+    //     '1d': '1day',
+    //     '1M': '1month',
+    //     '1y': '1year',
+    // },
     //
-    //
-    //
-    //
+    //Exchange Data Structure
+    //[
+    //     [
+    //         1504541580000, // UTC timestamp in milliseconds
+    //         4235.4,        // (O)pen price
+    //         4240.6,        // (H)ighest price
+    //         4230.0,        // (L)owest price
+    //         4230.7,        // (C)losing price
+    //         37.72941911    // (V)olume
+    //     ],
+    //     ...
+    // ]
+    // 
+    // Use Unix Date with 13 values when passing into request
+    // Example: 1512701458000
     //
     //**********************************
     app.get("/api/ccxt/:exchange/:symbol/:timeframe/:date/:limit", function(req, res) {
