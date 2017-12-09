e502a4faf0988d5e8a426b55f97bf4f6985d9a71
Evan Lu <evanlu7@gmail.com>
2017-12-08 16:05:30 -0800

    navbar transparency

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -45,3 +45,20 @@ function parallaxIt() {
 };
 
 parallaxIt();
+
+
+$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 10) {
+                    $("nav").css('background-color', 'white');
+                    $("nav a").css('color', 'black');
+                } else {
+                    $("nav").css('background-color', 'transparent');
+                    $("nav a").css('color', 'white');
+                }
+            });
+        });
+
+
