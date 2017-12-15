6b41186dd5980ea6545e2645a0007d9a96eb09fa
Evan Lu <evanlu7@gmail.com>
2017-12-14 18:26:19 -0800

    mobile friendly, proper colors

---
+++ public/assets/js/index2.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){       
+//             var scroll_pos = 0;
+//             $(document).scroll(function() { 
+//                 scroll_pos = $(this).scrollTop();
+//                 if(scroll_pos > 50) {
+//                     $("nav").css('background-color', 'red');
+//                     $("nav a").css('color', 'white');
+//                 } else {
+//                     $("nav").css('background-color', 'black');
+//                     $("nav a").css('color', 'white');
+//                 }
+//             });
+//         });
